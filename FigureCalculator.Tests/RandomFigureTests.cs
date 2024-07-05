463cf4e4bc0582f97be8d6e85cb4e93f9d2408ef
Rinat <Rinat@DESKTOP-FIS9LMA>
2024-07-05 21:55:09 +0400

    Добавил новый класс тестов

---
+++ FigureCalculator.Tests/RandomFigureTests.cs
@@ -0,0 +1,37 @@
+﻿using FigureCalculator.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureCalculator.Tests
+{
+    [TestFixture]
+    public class RandomFigureTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CalculateAreaRandomFigureIsNotZero()
+        {
+            // arrange
+            var random = new Random();
+            int figureNumber = random.Next(1, 2);
+            IFigure figure = figureNumber switch
+            {
+                1 => new Circle(3),
+                2 => new Triangle(3, 4, 5),
+            };
+
+            // act
+            var figureArea = figure.CalculateArea();
+
+            // assert
+            Assert.NotZero(figureArea);
+        }
+    }
+}
